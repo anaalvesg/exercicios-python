c12641153d3c1439d1315a4159df3b43f92529f9
ani <129322594+anaalvesg@users.noreply.github.com>
2023-09-26 20:08:51 -0300

    exercicio 14

---
+++ ex14.py
@@ -0,0 +1,6 @@
+# escreva um programa que converta uma temperatura digitando em graus celsius e converta para graus fahrenheit.
+
+c = float(input('Informe a temperatura em graus Celsius: '))
+f = (c * 1.8) + 32
+
+print(f'A temperatura de {c}°C corresponde à {f}°F.')
