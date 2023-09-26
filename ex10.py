fdd6908a62c03c6cfd0169ac61e364399fadc05b
ani <129322594+anaalvesg@users.noreply.github.com>
2023-09-26 19:34:42 -0300

    exercicio 10

--- ex10.py
+++ ex10.py
@@ -1,7 +1,6 @@
 # crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
 carteira = float(input('Quanto dinheiro você tem na carteira? R$ '))
-dolar = 4.99
+dolar = carteira / 4.99
 
-x = carteira / dolar
-print('Você pode comprar {:.2f} dólares!' .format(x))
+print('Você pode comprar US$ {:.2f}!' .format(x))
